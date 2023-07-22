bd649182589be969723f60607a813353cee214dc
Aurehau <114416513+Aurehau@users.noreply.github.com>
2023-07-22 22:05:25 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ window.addEventListener('scroll', () =>{
     if (window.scrollY > 40){
         document.querySelector("main>div:nth-child(2)").classList.add("scroll");
         document.querySelector(".photo").classList.add("grandephoto");
-        document.querySelector(".droit").classList.add("Dscroll");
         document.querySelector("h3").classList.add("visible");
         document.querySelector(".texte").classList.add("txtvisible");
         document.querySelector("h2").classList.add("invisible");
@@ -13,7 +12,6 @@ window.addEventListener('scroll', () =>{
     else{
         document.querySelector("main>div:nth-child(2)").classList.remove("scroll");
         document.querySelector(".photo").classList.remove("grandephoto");
-        document.querySelector(".droit").classList.remove("Dscroll");
         document.querySelector("h3").classList.remove("visible");
         document.querySelector(".texte").classList.remove("txtvisible");
         document.querySelector("h2").classList.remove("invisible");
@@ -21,9 +19,19 @@ window.addEventListener('scroll', () =>{
     }
 });
 
+window.addEventListener('scroll', () =>{
+    if ((window.scrollY > 40)&&(window.innerWidth <= 800)){
+        let text = document.querySelector(".texte").innerHTML;
+        document.querySelector(".texteresponsive").innerHTML = text;
+        document.querySelector(".texteresponsive").classList.add("txtvisibleresponsive");
+    }
+    else{
+        document.querySelector(".texteresponsive").classList.remove("txtvisibleresponsive");
+    }
+});
 
 window.addEventListener('scroll', () =>{
-    if (window.scrollY > 10){
+    if ((window.scrollY > 10)&&(window.innerWidth > 600)){
         document.querySelector("header").classList.add("scrollhead");
         document.querySelector("main").classList.add("enhaut");
         document.querySelector("header>a:nth-child(2)").classList.add("décale");
